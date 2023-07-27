9004ec9943c84b0bd79c4843e8e967ac4d878028
Ghogare Sanket Vasant INPU <Sanket_Vasant.Ghogare@syngenta.com>
2023-07-27 23:37:44 +0530

    solve the error

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -56,7 +56,7 @@ function Dashboard() {
 						</ul>
 					</div>
 				</div>
-				<div class="col p-0 m-0">
+				<div className="col p-0 m-0">
 					<div className='p-2 d-flex justify-content-center shadow'>
 						<h4>Employee Management System</h4>						
 					</div>
